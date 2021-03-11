538b380ea657dab7255bf3f3d5d1778796af9777
Nahid2194 <nahidjc80@gmail.com>
2021-03-11 21:51:58 +0600

    JWT setup

--- MyRestAPI/MyRestAPI/settings.py
+++ MyRestAPI/MyRestAPI/settings.py
@@ -42,6 +42,11 @@ MIDDLEWARE = [
 ]
 CORS_ORIGIN_ALLOW_ALL = True
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
 
 ROOT_URLCONF = "MyRestAPI.urls"
 
