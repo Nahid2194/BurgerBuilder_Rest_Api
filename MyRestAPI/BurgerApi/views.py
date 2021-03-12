c2d237476ee8b4e3c9b246fe925b0fd34f3b1cf7
Nahid2194 <nahidjc80@gmail.com>
2021-03-12 15:34:57 +0600

    orders filter

--- MyRestAPI/BurgerApi/views.py
+++ MyRestAPI/BurgerApi/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 from .models import UserProfile, Order
@@ -12,4 +13,13 @@ class UserProfileViewSet(ModelViewSet):
 
 class OrderViewSet(ModelViewSet):
     serializer_class = OrderSerializer
-    queryset = Order.objects.all()
+    permission_classes = [
+        # IsAuthenticated,
+    ]
+
+    def get_queryset(self):
+        queryset = Order.objects.all()
+        id = self.request.query_params.get("id", None)
+        if id is not None:
+            queryset = queryset.filter(user__id=id)
+        return queryset
